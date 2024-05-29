da77332dbf62687599dbd92b5d6c8c1012d8f9f1
CarterSarkela <cartersarkela@gmail.com>
2024-05-29 16:09:15 -0700

    cleaned up server.js and deleted api route

--- server/server.js
+++ server/server.js
@@ -1,29 +1,23 @@
-const express = require('express')
-const path = require('path');
-
+const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
-
-const apiRouter = require('./routes/apiRouter')
 
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client')));
 
-
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
   app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
-  })
-  };
-  
-//api reqs
-app.get('/api', apiRouter);
-
+    return res
+      .status(200)
+      .sendFile(path.join(__dirname, '../client/index.html'));
+  });
+}
 //404 handler
 app.get('/', (req, res) => {
   res.status(404);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
